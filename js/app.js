ea9fde1ed44562aeda4e5171129e18891525f94d
Ali Abualhawa <Aliabualhawa7@gmail.com>
2023-01-05 13:07:31 -0600

    chapter 1 and 2

--- js/app.js
+++ js/app.js
@@ -23,19 +23,29 @@ function makeMiddleEarth() {
 
 	// 1. create a section tag with an id of middle-earth
 
+	const section = document.createElement('section')
+	section.id = 'middle-earth'
+
 	// 2. use a for loop to iterate over the lands array that does the following:
 
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
+	for (let i=0; i < lands.length; i++){
 
+	//   2a. creates an article tag (there should be one for each land when the loop is done)
+		const landArticle = document.createElement('article')
 	//   2b. gives each land article an `id` tag of the corresponding land name
+		landArticle.id = lands[i]
 
 	//   2c. includes an h1 with the name of the land inside each land article
+		const landname = document.createElement('h1')
+		landname.textContent = lands[i]
+		landArticle.appendChild(landname)
 
 	//   2d. appends each land to the middle-earth section
+			section.appendChild(landArticle)
 
 	// 3. append the section to the body of the DOM.
+}  document.body.appendChild(section)
 }
-
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 1 complete - Made Middle Earth".
 
@@ -47,8 +57,16 @@ function makeHobbits() {
 	console.log('2: makeHobbits')
 
 	// display an `unordered list` of hobbits in the shire
+	const ulhobbits = document.createElement('ul')
+	ulhobbits.id = 'hobbies'
 
 	// give each hobbit a class of `hobbit`
+	for(let i = 0; i < hobbits.length; i ++) {
+		const lihobbit = document.createElement( 'li')
+		lihobbit.className = 'hobbit'
+		lihobbit.textContent = hobbits [i]
+		ulhobbits.appendChild(lihobbit)
+	}
 
 	// hint: create a 'ul' outside the loop into which to append the 'li's
 
